25067ae7f497fd7dbe081987f531e571a9aa960d
Talen Fisher <talen.fisher@cythral.com>
2022-03-14 18:39:34 -0500

    add --load back

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 REPOSITORY_URI=$1
 GIT_COMMIT=$2
 
-docker buildx build --platform linux/arm64 --tag $REPOSITORY_URI:$GIT_COMMIT .
+docker buildx build --load --platform linux/arm64 --tag $REPOSITORY_URI:$GIT_COMMIT .
 docker tag $REPOSITORY_URI:$GIT_COMMIT $REPOSITORY_URI:latest
